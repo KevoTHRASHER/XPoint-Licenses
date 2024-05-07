2df84bf9299708aaf446d48b9a4c55e1d163a3e9
Kevo.THRASHER <rosxhead@gmail.com>
2024-05-06 23:54:24 -0600

    hostname error change to uname -n

--- KevoXPoint.sh
+++ KevoXPoint.sh
@@ -2,7 +2,7 @@
 # Purpose: Display the ARM CPU and GPU TEMPERATURE of Raspberry Pi 4 Model B or XPoint
 # Script creado by Kevo.THRASHER
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
-echo "$(date) @ $(hostname)"
+echo "$(date) @ $(uname -n)"
 echo "----------------------------------------"
 echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp)"
 echo "CPU => $((cpu/1000))'C"
